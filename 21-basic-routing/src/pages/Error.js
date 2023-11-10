1507da589f6f69db0934ed413683bab3217f3f44
Don Wood <donwood@gmail.com>
2023-11-09 21:59:00 -0600

    Added basic routing project.

---
+++ 21-basic-routing/src/pages/Error.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import MainNavigation from "../components/MainNavigation";
+
+function ErrorPage() {
+  return <Fragment>
+    <MainNavigation />
+    <main>
+      <h1>An error occured!</h1>
+      <p>Could not find this page!</p>
+    </main>
+  </Fragment>
+}
+
+export default ErrorPage;
---
+++ 21-basic-routing/src/pages/ProductDetail.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+
+import { Link, useParams } from "react-router-dom";
+
+function ProductDetailPage() {
+
+  const params = useParams();
+
+
+
+  return (
+    <Fragment>
+      <h1>Product Details!</h1>
+      <p>{params.productId}</p>
+      <p><Link to=".." relative="path">Back</Link></p>
+    </Fragment>
+  );
+}
+
+export default ProductDetailPage;
