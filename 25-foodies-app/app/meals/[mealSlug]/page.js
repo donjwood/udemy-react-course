3bbff3c674359f3440d3ba2f25ad39c2a3c016ef
Don Wood <donwood@gmail.com>
2024-04-10 16:23:52 -0500

    Added Metadata.

--- 25-foodies-app/app/meals/[mealSlug]/page.js
+++ 25-foodies-app/app/meals/[mealSlug]/page.js
@@ -1,23 +1,33 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import classes from './page.module.css';
-import { getMeal } from '@/lib/meals';
-import { notFound } from 'next/navigation';
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+  
+  return { title: meal.title, description: meal.summary };
+}
 
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
 
-  if(!meal) {
+  if (!meal) {
     notFound();
   }
 
-  meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image src={meal.image} alt={meal.title} fill/>
+          <Image src={meal.image} alt={meal.title} fill />
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
@@ -28,9 +38,12 @@ export default function MealDetailsPage({ params }) {
         </div>
       </header>
       <main>
-        <p className={classes.instructions} dangerouslySetInnerHTML={{
-          __html: meal.instructions,
-        }}></p>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
       </main>
     </>
   );
