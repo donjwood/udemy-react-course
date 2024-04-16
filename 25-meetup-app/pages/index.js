8f88aacc3458b32dd01a6486cf0558fd949722df
Don Wood <donwood@gmail.com>
2024-04-16 17:17:15 -0500

    Updated index page.

--- 25-meetup-app/pages/index.js
+++ 25-meetup-app/pages/index.js
@@ -40,7 +40,7 @@ function HomePage(props) {
 export async function getStaticProps() {
   // fetch data from an API
   const client = await MongoClient.connect(
-    "mongodb+srv://donwood:7DhgTWsRYEuPKYIW@cluster0.s3u4mdu.mongodb.net/meetups?retryWrites=true&w=majority"
+    "mongodb+srv://donwood:<Password>@cluster0.s3u4mdu.mongodb.net/meetups?retryWrites=true&w=majority"
   );
   const db = client.db();
 
